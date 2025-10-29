b5c55e1aab2053b2486b6c0ddbf9c6dfcb38a233
harshil-netai <harshil.patel@netanalytics.ai>
2025-10-29 18:23:36 +0530

    Complete till chapter 11 Search & paginaiton

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+export const config = {
+  dynamic: 'force-dynamic', // This is the preferred location for configuration
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
